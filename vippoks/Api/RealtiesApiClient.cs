4c1973bfa361fac8575e8f301301aecb0e966244
TheEvilFace <61375405+TheEvilFace@users.noreply.github.com>
2020-05-28 22:43:16 +0400

    dsadsadasdssa

---
+++ vippoks/Api/RealtiesApiClient.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace vippoks
+{
+    public sealed class RealtiesApiClient : BaseApiClient
+    {
+        private string REALTIES_API_URL = API_BASE_URL + "/realties";
+
+        public ApiDefaultResponse Get()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(REALTIES_API_URL + @"/get");
+            request.ContentType = CONTENT_TYPE;
+            request.Method = METHOD_GET;
+            string res = this.makeRequest(request);
+            return JsonConvert.DeserializeObject<ApiDefaultResponse>(res);
+        }
+
+        public void Delete(int id)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(REALTIES_API_URL + @"/delete/" + id);
+            request.ContentType = CONTENT_TYPE;
+            request.Method = METHOD_GET;
+            this.makeRequest(request);
+        }
+
+        public void UpdateById(int id, string city, string street, string house, int type_id, int flat, float area, int floors_count, string lat, string Long)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(REALTIES_API_URL + @"/update/" + id);
+            request.ContentType = CONTENT_TYPE;
+            request.Method = METHOD_POST;
+
+            object clientDataRequest = new { city, street, house, type_id, flat, area, floors_count, lat, Long };
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+              
+            }
+            var sad = (JsonConvert.SerializeObject(clientDataRequest));
+
+            this.makeRequest(request);
+        }
+
+        public void Create(int id, string city, string street, string house, int type_id, int flat, float area, int floors_count, string lat, string Long)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(REALTIES_API_URL + @"/create");
+
+            request.ContentType = CONTENT_TYPE;
+            request.Method = METHOD_POST;
+
+            object clientDataRequest = new { city, street, house, type_id, flat, area, floors_count, lat, Long };
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(JsonConvert.SerializeObject(clientDataRequest));
+            }
+
+            this.makeRequest(request);
+        }
+    }
+}
---
+++ vippoks/Auth.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace vippoks
+{
+    public partial class Auth : Form
+    {
+        private readonly AuthApiClient _authApiClient;
+        
+        public Auth()
+        {
+            InitializeComponent();
+            _authApiClient = new AuthApiClient();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isAuth = _authApiClient.Auth(textBox1.Text, textBox2.Text);
+            if (isAuth)
+            {
+                Главное_меню f1 = new Главное_меню();
+                f1.Show();
+            }else {
+                MessageBox.Show("Пароль и/или логин неверны!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
